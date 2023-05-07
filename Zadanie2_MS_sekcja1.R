4fe534f02d60dfedd25480d1da36e12401e93f17
julesmuskala <30392252+julesmuskala@users.noreply.github.com>
2023-05-07 22:19:23 +0200

    zadanie 2

--- Zadanie2_MS_sekcja1.R
+++ Zadanie2_MS_sekcja1.R
@@ -1,4 +1,10 @@
 #Zadanie 2
+#Wczytanie danych z pliku .csv
+wyniki <- read.csv("data/data.csv")
+
+#Podział na grupy
+grupa1 <- subset(wyniki,Group == 1)$Score
+grupa2 <- subset(wyniki,Group == 2)$Score
 
 install.packages("nortest")
 
@@ -13,14 +19,14 @@ cat("Wartość p-value dla grupy 1:", test1$p.value, "\n")
 cat("Wartość p-value dla grupy 2:", test2$p.value, "\n")
 
 # Interpretacja wyników
-if (test1$p.value < 0.05) {
-  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa można stwierdzić, że wyniki sprawdzianów w grupie 1 nie mają rozkładu normalnego.\n")
+if (test1$p.value > 0.05) {
+  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa można stwierdzić, że wyniki sprawdzianów w grupie 1 mają rozkład normalny.\n")
 } else {
-  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa nie ma podstaw do odrzucenia hipotezy o normalności rozkładu wyników sprawdzianów w grupie 1.\n")
+  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa można stwierdzić, że wyniki sprawdzianów w grupie 1 nie mają rozkładu normalnego. 1.\n")
 }
 
-if (test2$p.value < 0.05) {
-  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa można stwierdzić, że wyniki sprawdzianów w grupie 2 nie mają rozkładu normalnego.\n")
+if (test2$p.value > 0.05) {
+  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa można stwierdzić, że wyniki sprawdzianów w grupie 2 mają rozkład normalny.\n")
 } else {
-  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa nie ma podstaw do odrzucenia hipotezy o normalności rozkładu wyników sprawdzianów w grupie 2.\n")
+  cat("Na podstawie testu zgodności Kolmogorowa-Lillieforsa można stwierdzić, że wyniki sprawdzianów w grupie 1 nie mają rozkładu normalnego.\n")
 }
