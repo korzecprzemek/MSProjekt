e16de41a90fc0f1ff82783e7568edd1cd8efdcb9
heckooo <phecko02@gmail.com>
2023-05-24 23:06:31 +0200

    Poprawione 4

--- Zadanie4_MS_sekcja1.R
+++ Zadanie4_MS_sekcja1.R
@@ -4,34 +4,18 @@
 wyniki <- read.csv("data/data.csv")
 data <- subset(wyniki, Group == 2)$Score
 
-# Liczność próby
 n <- length(data)
-
-# Średnia arytmetyczna próby
-x_mean <- mean(data)
-
-# Estymator odchylenia standardowego próby
-s <- sqrt((1/(n-1)) * sum((data - x_mean)^2))
-
-# Wartość hipotetyczna
-sigma <- 20
+srednia <- mean(data)
+wariancja <- sum((data - srednia)^2) / (n-1)
+p_rozklad_975 <- qchisq(1 - 0.05/ 2, n-1, lower.tail = TRUE)
+p_rozklad_025 <- qchisq(0.05/ 2, n-1, lower.tail = TRUE)
 
 # Statystyka testowa
-chi_square <- (n - 1) * s^2 / sigma^2
+u <- (n * wariancja) / (20^2)
 
-# Stopnie swobody
-df <- n - 1
-print(df)
-
-# Wartość p
-p_value <- 1 - pchisq(chi_square, df)
-print(p_value)
-
-# Wynik testu hipotezy
-if(p_value > 0.05){
+# Sprawdzamy czy statystyka testowa nalezy do obszaru krytycznego
+if (u > p_rozklad_025 & u < p_rozklad_975) {
   cat("Odrzucamy hipotezę zerową, odchylenie standardowe nie wynosi 20.")
 }else{
   cat("Nie ma podstaw do odrzucenia hipotezy zerowej, odchylenie standardowe wynosi 20.")
 }
-
-
